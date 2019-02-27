e15cef89668c48bf44204dbe346a101fb1874fc7
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-27 16:13:44 -0700

    Added routes and make sure that token is in local storage before going to home view

--- users-client/src/App.js
+++ users-client/src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
-import "./App.css";
+import { Route, Redirect } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>App</h1>
-      </div>
-    );
-  }
-}
+import "./App.css";
+import HomeView from "./views/HomeView";
+import LoginView from "./views/LoginView";
+import RegisterView from "./views/RegisterView";
+
+const App = props => {
+  const token = localStorage.getItem("jwt");
+  console.log("token: ", token);
+
+  return (
+    <div className="App">
+      <Route
+        exact
+        path="/"
+        render={props =>
+          token ? <HomeView {...props} /> : <Redirect to="/login" />
+        }
+      />
+      <Route exact path="/login" render={props => <LoginView {...props} />} />
+      <Route
+        exact
+        path="/register"
+        render={props => <RegisterView {...props} />}
+      />
+    </div>
+  );
+};
 
 export default App;
---
+++ users-client/src/views/LoginView.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginView = props => {
+  return (
+    <div>
+      <h1>Login View</h1>
+    </div>
+  );
+};
+
+export default LoginView;
