cbcd1aa7a2a8f2b8cba410733ae00d090546b2ee
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-27 16:01:04 -0700

    Fix typo ins cors import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
-cosnt cors = require('cors');
+const cors = require('cors');
 
 const authRouter = require('./auth/authRouter');
 const usersRouter = require('./users/usersRouter');
