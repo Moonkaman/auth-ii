795a20609be13f1fb70dad31c40ae3e0ba45ebdc
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-28 15:25:46 -0700

    Add password bcrypt compare that I forgot to before

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -34,7 +34,7 @@ router.post('/login', (req, res) => {
   } else {
     db.getBy({username: req.body.username})
       .then(user => {
-        if(user) {
+        if(user && bcrypt.compareSync(req.body.password, user.password)) {
           const token = generateToken(user);
           res.status(200).json({message: `Welcome ${user.username}`, token})
         } else {
